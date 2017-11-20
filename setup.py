93333256d47810747691b7d244cbc9f44219eb19
Tyler <1tsantos7@gmail.com>
2017-11-19 19:00:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-  name = 'wunder',
+  name = 'WunderWeather',
   packages = ['wunder'],
   install_requires = ['easydict','requests'],
   version = '0.1',
   description = 'Wrapper for Weather Underground API',
   author = 'Tyler Santos',
   author_email = '1tsantos7+wunderweather@gmail.com',
-  url = '', # use the URL to the github repo
+  url = 'https://github.com/T-Santos/WunderWeather',
   keywords = ['weather', 'wunderground'],
   classifiers = [
     'Development Status :: 3 - Alpha',
